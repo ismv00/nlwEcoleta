1e0a7acaa391853e3bd8716394cd789adcab3db1
Igor Menezes <ismv00@icloud.com>
2020-06-08 00:20:16 -0400

    e-coleta

---
+++ public/scripts/intro.js
@@ -0,0 +1,41 @@
+// // // document.write("hello")
+
+// // // let myvar 
+// // // myvar = "He"
+
+// // // const myconst = "he"
+
+// // // document.write(myconst + myvar)
+// // const pessoa ={
+// //   altura: "1,80",
+// //   idade: 24,
+// //   solteiro: true,
+// //   correr() {
+// //     document.write("executar uma grande logica de correr")
+// //   } 
+  
+// // }
+
+// // pessoa.correr()
+
+// // const colecaoDeBolinhas = ["blue", "green", 1, {name: "myName"}]
+
+// // document.write(colecaoDeBolinhas[3].name)
+
+// // function sayMyName(name){
+// //   document.write(name)
+// // }
+
+// // sayMyName("Igor")
+
+// // const notaFinal = 6
+
+// // if(notaFinal >= 7) {
+// //   document.write("aprovado")
+// // }else {
+// //   document.write("reprovado")
+// // }
+
+// for (i = 0; i<10; i++) {
+//     document.write(`<p>${i}</p>`)
+//   }
